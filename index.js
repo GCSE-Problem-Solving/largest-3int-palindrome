690d2a62c079edf2bac226b3aa9cbd4658f6bdd3
37bird <39220220+37bird@users.noreply.github.com>
2018-10-30 20:25:39 +0000

    Update index.js
    added moar comments

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 for (let x = 999; x >= 100; x--) {
 
     for (let y = 999; y >= 100; y--) {
-        let z = x * y;
-        let str = z.toString();
-        let readyToBeReversed = str.split("");
-        let reversedArray = readyToBeReversed.reverse();
-        let reversedString = reversedArray.join("");
-        let reversedNumber = parseInt(reversedString);
-        if(z === reversedNumber) {
-            console.log("palindrome found: " + z)
+        //loop through all the possible 3 integer products 
+        let z = x * y; //all the three integer products
+        let str = z.toString(); //changes the number to a string to be split
+        let readyToBeReversed = str.split(""); //splits the string into an array of letter-long strings
+        let reversedArray = readyToBeReversed.reverse(); //puts all the strings in backwards order
+        let reversedString = reversedArray.join(""); //joins the strings back together into one whole string
+        let reversedNumber = parseInt(reversedString); //changes string to a number
+        if(z === reversedNumber) { 
+            console.log("palindrome found: " + z) //if the number is a palindrome, log it to the console.
         }
     }
 

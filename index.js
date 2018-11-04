67589227e1b9f871585e5bf954a2e79c6cdf5eec
37bird <39220220+37bird@users.noreply.github.com>
2018-11-04 17:35:22 +0000

    added comments
    now readable!!

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 loop1:
 for (let x = 999; x >= 100; x--) {
     loop2:
-    for (let y = 999; y >= 100; y--) {
-        let z = x * y;
-        let str = z.toString();
-        let readyToBeReversed = str.split("");
-        let reversedArray = readyToBeReversed.reverse();
-        let reversedString = reversedArray.join("");
-        let reversedNumber = parseInt(reversedString);
-        if(z === reversedNumber & z === 906609) {
-            console.log("palindrome found: " + z)
+    for (let y = 999; y >= 100; y--) { // loops through all the three integer number sets
+        let z = x * y; // multiplies x and y
+        let str = z.toString(); //m akes the number a string for easier rearrangement
+        let readyToBeReversed = str.split(""); // splits the number into one character arrays
+        let reversedArray = readyToBeReversed.reverse(); // reverses the set of arrays
+        let reversedString = reversedArray.join(""); // joins the arrays into a string
+        let reversedNumber = parseInt(reversedString); // makes the string a number
+        if(z === reversedNumber & z === 906609) { // checks if the number is a palindrome and fixes a bug where the highest palindrome shows later
+            console.log("palindrome found: " + z) //logs the palindrome to the console and stops the loops
             break loop1;
             break loop2;
         }

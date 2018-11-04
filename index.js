37f1590dfafa58064ff9577a56129529b8fa828e
37bird <39220220+37bird@users.noreply.github.com>
2018-11-04 17:31:11 +0000

    fixed another bug
    fixed a bug where a lower number appeared before the highest palindrome

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+loop1:
 for (let x = 999; x >= 100; x--) {
-
+    loop2:
     for (let y = 999; y >= 100; y--) {
-        //loop through all the possible 3 integer products 
-        let z = x * y; //all the three integer products
-        let str = z.toString(); //changes the number to a string to be split
-        let readyToBeReversed = str.split(""); //splits the string into an array of letter-long strings
-        let reversedArray = readyToBeReversed.reverse(); //puts all the strings in backwards order
-        let reversedString = reversedArray.join(""); //joins the strings back together into one whole string
-        let reversedNumber = parseInt(reversedString); //changes string to a number
-        if(z === reversedNumber) { 
+        let z = x * y;
+        let str = z.toString();
+        let readyToBeReversed = str.split("");
+        let reversedArray = readyToBeReversed.reverse();
+        let reversedString = reversedArray.join("");
+        let reversedNumber = parseInt(reversedString);
+        if(z === reversedNumber & z === 906609) {
             console.log("palindrome found: " + z)
-            break;//if the number is a palindrome, log it to the console then stop the code.
+            break loop1;
+            break loop2;
         }
     }
 
